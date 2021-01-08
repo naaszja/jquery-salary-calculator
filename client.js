7f4c82e566d2b488dfc95c5e74c29b3dbab479a1
Jamie Naasz <naaszja@gmail.com>
2021-01-08 17:01:38 -0600

    beginnings of table created. minimal styling

--- client.js
+++ client.js
@@ -4,4 +4,22 @@ $(onReady)
 
 function onReady() {
     console.log('JQ linked');
+
+    //Create employee object
+    let employee = {
+        firstName: '',
+        lastName: '',
+        idNumber: 0,
+        jobTitle: '',
+        annualSalary: 0
+    };
+
+    //Create array to hold employee objects
+    let employees = [];
+
+    //Create table to display employee objects
+
+
+    //Create inputs to collect employee information
+
 }
